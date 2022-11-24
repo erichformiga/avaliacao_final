c23b687e2b8a48e667fbf4650c8e47bc08a9a649
erichformiga <erichformiga@gmail.com>
2022-11-24 13:05:35 -0300

    entrega 24/11

--- Flysys.java
+++ Flysys.java
@@ -4,22 +4,27 @@ import javax.swing.*;
 /*teste  novo*/
 public class Flysys extends JFrame {
 
+    // Itens SubMenu Cadastro
     private JMenuItem aeronaveMenu = new JMenuItem("Aeronave");
     private JMenuItem companhiaMenu = new JMenuItem("Companhia");
     private JMenuItem hangaresMenu = new JMenuItem("Hangares");
     private JMenuItem pistaMenu = new JMenuItem("Pista");
     private JMenuItem sairMenu = new JMenuItem("Sair...");
-
+    
+    // Itens SubMenu Operações
+    private JMenuItem gerarvoosMenu = new JMenuItem("Gerar Voos");
     private JMenuItem operacoesMenu = new JMenuItem("Relatório");
     
+    // Menu Cadastro
     private JMenu menuCadastro = new JMenu("Cadastro");
 
-    private JMenu menuRelatorio = new JMenu("Operações");
+    // Menu Operações
+    private JMenu menuOperacoes = new JMenu("Operações");
 
-    private JMenuBar menuBar = new JMenuBar();
+    private JMenuBar menuPrincipal = new JMenuBar();
 
     public Flysys() {
-
+        // Itens Menu Cadastro
         menuCadastro.add(companhiaMenu);
         menuCadastro.add(aeronaveMenu);
         menuCadastro.addSeparator();
@@ -27,19 +32,22 @@ public class Flysys extends JFrame {
         menuCadastro.add(pistaMenu);
         menuCadastro.addSeparator();
         menuCadastro.add(sairMenu);
-
-        menuRelatorio.add(operacoesMenu);
+        // Itens Menu Operações
+        menuOperacoes.add(gerarvoosMenu);
+        menuCadastro.addSeparator();
+        menuOperacoes.add(operacoesMenu);
     
+        // Itens Menu Principal
+        menuPrincipal.add(menuCadastro);
+        menuPrincipal.add(menuOperacoes);
 
-        menuBar.add(menuCadastro);
-        menuBar.add(menuRelatorio);
-
-        this.setJMenuBar(menuBar);
+        this.setJMenuBar(menuPrincipal);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Flysys :: Sistema de Gestão de Aeronves");
+        //this.setIconImage("c:/erich/aviao_icon.png");
         this.setSize(500,300);
         this.setLocation(600, 300);
-        //this.setBounds(MAXIMIZED_BOTH, ABORT, WIDTH, HEIGHT);
+        //this.setBounds(500, 500, 500, 500);
         this.setVisible(true);
     }
 
