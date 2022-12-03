29742062a5bb7181be6d1766c98684dbfd079516
erichformiga <erichformiga@gmail.com>
2022-12-03 18:50:07 -0300

    entrega 02/12

--- Companhia.java
+++ Companhia.java
@@ -1,99 +1,80 @@
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-import javax.swing.plaf.FontUIResource;
+import java.util.Objects;
 
-public class Companhia extends JFrame {
-    private Font fontForm = new FontUIResource("consolas", Font.PLAIN, 12);
+public class Companhia {
+    private int id;
+    private String nome;
+    private String cnpj;
+
 
     public Companhia() {
-
-    /*  ID_COMPANHIA
-	    NOME_COMPANHIA
-        CNPJ
-        DATA */
-
-        JLabel labelId = new JLabel("ID..:");
-        labelId.setFont(fontForm);
-        JLabel labelNome = new JLabel("Nome:");
-        labelNome.setFont(fontForm);
-        JLabel labelCnpj = new JLabel("CNPJ:");
-        labelCnpj.setFont(fontForm);
-        JLabel labelData = new JLabel("Data:");
-        labelData.setFont(fontForm);
-        
-        //Posicionando os labels na tela
-        JPanel formCompahia = new JPanel();
-        formCompahia.setLayout(new GridLayout(3, 1, 10, 10));
-        JPanel panel1 = new JPanel(new GridLayout(4, 2, 10, 10));
-        JPanel panel2 = new JPanel();
-        JPanel panel3 = new JPanel();
-        
-        formCompahia.add(panel1);
-        formCompahia.add(panel2);
-        formCompahia.add(panel3);
-
-        panel1.add(labelId);
-        panel1.add(labelNome);
-        panel1.add(labelCnpj);
-        panel1.add(labelData);
-
-        //Adicionando os botões
-        JButton bntNovo = new JButton("Novo");
-        bntNovo.setFont(fontForm);
-        
-        JButton bntListar = new JButton("Listar");
-        bntListar.setFont(fontForm);
-        JButton bntCancelar = new JButton("Cancelar");
-        bntCancelar.setFont(fontForm);
-        JButton bntFecha = new JButton("Fechar");
-        bntCancelar.setFont(fontForm);
-
-        String[] columnNames = {"First Name",
-                        "Last Name",
-                        "Sport",
-                        "# of Years",
-                        "Vegetarian"};
-
-        Object[][] data = {
-            {"Kathy", "Smith",
-             "Snowboarding", new Integer(5), new Boolean(false)},
-            {"John", "Doe",
-             "Rowing", new Integer(3), new Boolean(true)},
-            {"Sue", "Black",
-             "Knitting", new Integer(2), new Boolean(false)},
-            {"Jane", "White",
-             "Speed reading", new Integer(20), new Boolean(true)},
-            {"Joe", "Brown",
-             "Pool", new Integer(10), new Boolean(false)}
-        };
-        JTable tableView = new JTable(data, columnNames);
-        tableView.setAutoResizeMode( JTable.AUTO_RESIZE_OFF );
-        tableView.setPreferredScrollableViewportSize(new Dimension(400,100));
-        JScrollPane scrollPane = new JScrollPane(tableView);
-        panel2.add(scrollPane);
-
-        panel3.add(bntListar);
-        panel3.add(bntCancelar);
-        panel3.add(bntFecha);
-
-        // Inicializando o Form Compahia
-        add(formCompahia, BorderLayout.NORTH);
-
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setTitle("Flysys :: Cadastro de Companhias");
-        this.setSize(500,400);
-        this.setLocation(600, 300);
-        this.setLocationRelativeTo(null);
-        this.setResizable(true);
-        //this.setBounds(MAXIMIZED_BOTH, ABORT, WIDTH, HEIGHT);
-        this.setVisible(true);    
     }
 
-    //Conexão com o Banco de dados
+    public Companhia(String nome, String cnpj) {
+        this.nome = nome;
+        this.cnpj = cnpj;
+        // INTEGRAR COM O BANCO
 
-
-    public static void main(String[] args){
-        Companhia forCompanhia = new Companhia();
+        this.id = 1;
     }
+
+    public Companhia(int id, String nome, String cnpj) {
+        this.id = id;
+        this.nome = nome;
+        this.cnpj = cnpj;
+    }
+    public int getId() {
+        return this.id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return this.nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCnpj() {
+        return this.cnpj;
+    }
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+    public Companhia id(int id) {
+        setId(id);
+        return this;
+    }
+    public Companhia nome(String nome) {
+        setNome(nome);
+        return this;
+    }
+    public Companhia cnpj(String cnpj) {
+        setCnpj(cnpj);
+        return this;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Companhia)) {
+            return false;
+        }
+        Companhia companhia = (Companhia) o;
+        return id == companhia.id && Objects.equals(nome, companhia.nome) && Objects.equals(cnpj, companhia.cnpj);
+    }
+    
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome, cnpj);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", nome='" + getNome() + "'" +
+            ", cnpj='" + getCnpj() + "'" +
+            "}";
+    }
+
 }
---
+++ CompanhiaView.java
@@ -0,0 +1,125 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.plaf.FontUIResource;
+
+public class CompanhiaView extends JFrame {
+    private Font fontForm = new FontUIResource("consolas", Font.PLAIN, 12);
+
+    public CompanhiaView() {
+        this(0);
+    }
+
+    public CompanhiaView(int id) {
+
+    /*  ID_COMPANHIA
+	    NOME_COMPANHIA
+        CNPJ
+        DATA */
+
+        JLabel labelId = new JLabel("ID..:");
+        labelId.setFont(fontForm);
+        JLabel labelNome = new JLabel("Nome:");
+        labelNome.setFont(fontForm);
+        JLabel labelCnpj = new JLabel("CNPJ:");
+        labelCnpj.setFont(fontForm);
+        JLabel labelData = new JLabel("Data:");
+        labelData.setFont(fontForm);
+        
+        //Posicionando os componetes na tela
+        JPanel formCompahia = new JPanel();
+        formCompahia.setLayout(new GridLayout(3, 1, 10, 10));
+        JPanel panel1 = new JPanel(new GridLayout(4, 4, 20, 10));
+        JPanel panel2 = new JPanel();
+        JPanel panel3 = new JPanel();
+
+        JTextField textId = new JTextField(25);
+        JTextField textNome = new JTextField(25);
+        JTextField textCnpj = new JTextField(25);
+        JTextField textData = new JTextField(25);
+
+        // Se o id != 0 significa que é Alteração
+        if (id !=0) {
+            Companhia companhia = new Companhia("Gol", "1144741");
+
+            textNome.setText(companhia.getNome());
+            textCnpj.setText(companhia.getCnpj());
+        }
+        formCompahia.add(panel1);
+        formCompahia.add(panel2);
+        formCompahia.add(panel3);
+
+        panel1.add(labelId);
+        panel1.add(textId);
+        panel1.add(labelNome);
+        panel1.add(textNome);
+        panel1.add(labelCnpj);
+        panel1.add(textCnpj);
+        panel1.add(labelData);
+        panel1.add(textData);
+
+        //Adicionando os botões
+        JButton bntNovo = new JButton("  Novo  ");
+        bntNovo.setFont(fontForm);
+        JButton bntListar = new JButton("Listar");
+        bntListar.setFont(fontForm);
+        JButton bntCancelar = new JButton("Cancelar");
+        bntCancelar.setFont(fontForm);
+        JButton bntGravar = new JButton("Gravar");
+        bntGravar.setFont(fontForm);
+        JButton bntFechar = new JButton("Fechar");
+        bntFechar.setFont(fontForm);
+
+        panel3.add(bntNovo);
+        panel3.add(bntListar);
+        panel3.add(bntCancelar);
+        panel3.add(bntGravar);
+        panel3.add(bntFechar);
+
+        String[] columnNames = {"Campo",
+                        "Last Name",
+                        "Sport",
+                        "# of Years",
+                        "Vegetarian"};
+
+        Object[][] data = {
+            {"Kathy", "Smith",
+             "Snowboarding", new Integer(5), new Boolean(false)},
+            {"John", "Doe",
+             "Rowing", new Integer(3), new Boolean(true)},
+            {"Sue", "Black",
+             "Knitting", new Integer(2), new Boolean(false)},
+            {"Jane", "White",
+             "Speed reading", new Integer(20), new Boolean(true)},
+            {"Joe", "Brown",
+             "Pool", new Integer(10), new Boolean(false)}
+        };
+        JTable tableView = new JTable(data, columnNames);
+        tableView.setAutoResizeMode( JTable.AUTO_RESIZE_OFF );
+        tableView.setPreferredScrollableViewportSize(new Dimension(400,100));
+        JScrollPane scrollPane = new JScrollPane(tableView);
+        panel2.add(scrollPane);
+
+
+
+        // Inicializando o Form Compahia
+        add(formCompahia, BorderLayout.NORTH);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Flysys :: Cadastro de Companhias");
+        this.setSize(500,400);
+        this.setLocation(600, 300);
+        this.setLocationRelativeTo(null);
+        this.setResizable(true);
+        //this.setBounds(MAXIMIZED_BOTH, ABORT, WIDTH, HEIGHT);
+        this.setVisible(true);    
+    }
+
+    //Conexão com o Banco de dados
+
+
+    public static void main(String[] args){
+        // CompanhiaView forCompanhia = new CompanhiaView();
+        CompanhiaView forCompanhiaUpdate = new CompanhiaView(1);
+    }
+}
